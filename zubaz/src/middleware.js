67e191db5a6fa1819e47ab26be0c3bd33c646d4e
Rohan Gope <rg2512@it.jgec.ac.in>
2024-09-12 10:58:30 +0530

    fixing bug

--- zubaz/src/middleware.js
+++ zubaz/src/middleware.js
@@ -16,18 +16,18 @@ export function middleware(req) {
     return NextResponse.rewrite(newUrl);
   }
 
-  const hostname = req.nextUrl.hostname;
+  // const hostname = req.nextUrl.hostname;
 
-  // Extract subdomain from the hostname
-  const subdomain = hostname.split(".")[0];
+  // // Extract subdomain from the hostname
+  // const subdomain = hostname.split(".")[0];
 
-  // If it's not localhost and there is a subdomain (e.g., test.localhost)
-  if (hostname !== "localhost" && subdomain !== "localhost") {
-    // Rewrite to the dynamic subdomain route
-    const url = req.nextUrl.clone();
-    url.pathname = `/${subdomain}`; // Map to /[subdomain] page
-    return NextResponse.rewrite(url);
-  }
+  // // If it's not localhost and there is a subdomain (e.g., test.localhost)
+  // if (hostname !== "localhost" && subdomain !== "localhost") {
+  //   // Rewrite to the dynamic subdomain route
+  //   const url = req.nextUrl.clone();
+  //   url.pathname = `/${subdomain}`; // Map to /[subdomain] page
+  //   return NextResponse.rewrite(url);
+  // }
 
   return NextResponse.next(); // Proceed normally for non-subdomains
 }
